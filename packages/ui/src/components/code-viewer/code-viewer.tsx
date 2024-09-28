268e996dc916ecee8482766dca8275dfbc8a0845
Kuba Janik <jakubjanik00@gmail.com>
2024-09-28 09:36:10 +0200

    feat: add line numbers to code viewer

--- packages/ui/src/components/code-viewer/code-viewer.tsx
+++ packages/ui/src/components/code-viewer/code-viewer.tsx
@@ -10,7 +10,7 @@ interface Props {
 export const CodeViewer = ({ sourceCode, currentLine }: Props) => {
   return (
     <div className="h-full relative border border-neutral-100 border-r-0">
-      <pre className="absolute top-0 w-full p-4 text-xs overflow-auto">
+      <pre className="absolute top-0 w-full p-2 pl-8 text-xs overflow-auto">
         <code dangerouslySetInnerHTML={{ __html: sourceCode }} />
       </pre>
 
@@ -23,13 +23,13 @@ const LineHighlight = ({ sourceCode, currentLine }: Props) => {
   const lineNumbers = getLineNumbers(sourceCode)
 
   return (
-    <pre className="pointer-events-none select-none py-4 text-xs">
+    <pre className="pointer-events-none select-none py-2 text-xs">
       {lineNumbers.map((line) => (
         <Fragment key={line}>
           <span
-            className={`inline-block w-full ${currentLine === line ? "bg-neutral-100" : ""}`}
+            className={`inline-block w-full pl-2 text-neutral-500 ${currentLine === line ? "bg-neutral-100" : ""}`}
           >
-            {" "}
+            <span className="inline-block w-3 text-right">{line}</span>
           </span>
           {"\n"}
         </Fragment>
