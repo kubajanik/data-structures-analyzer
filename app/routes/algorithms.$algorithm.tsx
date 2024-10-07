2ec279a52af7d2489fa4b7f9c6a487f985b95532
Kuba Janik <jakubjanik00@gmail.com>
2024-10-07 10:30:19 +0200

    fix: ts issue

--- app/routes/algorithms.$algorithm.tsx
+++ app/routes/algorithms.$algorithm.tsx
@@ -1,38 +1,38 @@
-import React from "react"
+import React from "react";
 
-import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node"
-import { json, useLoaderData } from "@remix-run/react"
+import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
+import { json, useLoaderData } from "@remix-run/react";
 
 import {
   SplitView,
   CodeViewer,
   VisualizationCanvas,
   StepsPanel,
-} from "~/components"
-import { AlgorithmData } from "~/types"
-import { db } from "~/utils/db.server"
+} from "~/components";
+import { AlgorithmData } from "~/types";
+import { db } from "~/utils/db.server";
 
 export const meta: MetaFunction = () => {
   return [
     { title: "Algorithm" },
     { name: "description", content: "Algorithm view" },
-  ]
-}
+  ];
+};
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const algorithm = await db
     .collection<AlgorithmData>("algorithms")
-    .findOne({ id: params.algorithm })
+    .findOne({ id: params.algorithm });
 
-  return json({ algorithm })
-}
+  return json({ algorithm });
+};
 
 export default function AlgorithmRoute() {
-  const { algorithm } = useLoaderData<typeof loader>()
-  const [currentStep, setCurrentStep] = React.useState(0)
+  const { algorithm } = useLoaderData<typeof loader>();
+  const [currentStep, setCurrentStep] = React.useState(0);
 
   if (!algorithm) {
-    return null
+    return null;
   }
 
   return (
@@ -56,5 +56,5 @@ export default function AlgorithmRoute() {
         onStepChange={setCurrentStep}
       />
     </div>
-  )
+  );
 }
